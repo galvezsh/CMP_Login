7a323411db47ec9897196c01b79ef490c56e1050
Galvezsh <galvezssr@gmail.com>
2025-10-27 15:25:42 +0100

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/com/galvezsh/cmplogin/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.galvezsh.cmplogin
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
---
+++ composeApp/src/jvmMain/kotlin/com/galvezsh/cmplogin/main.kt
@@ -0,0 +1,13 @@
+package com.galvezsh.cmplogin
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "CMP_Login",
+    ) {
+        App()
+    }
+}
